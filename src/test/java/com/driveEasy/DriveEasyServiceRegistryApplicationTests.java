697c1fb9e7e1796217791a24cb4917deddf40ace
RachnaRaghu <50066933+RachnaRaghu@users.noreply.github.com>
2026-06-22 19:48:57 +0530

    Add files via upload

---
+++ src/test/java/com/driveEasy/DriveEasyServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driveEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveEasyServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
